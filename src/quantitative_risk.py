2d4f4c83a87d17cb986c7d9911b0f89fbfdbb4ad
Martin Vesterlund <Otyg@users.noreply.github.com>
2026-01-28 23:09:40 +0100

    Initial

---
+++ src/quantitative_risk.py
@@ -0,0 +1,77 @@
+#
+# BSD 3-Clause License
+#
+# Copyright (c) 2026, Martin Vesterlund
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+from decimal import *
+
+from riskBase.src.montecarlo import MonteCarloRange, MonteCarloSimulation
+
+
+class Risk():
+    def __init__(self, values: dict = None):
+        if not values:
+            self.threat_event_frequency = MonteCarloRange(probable=Decimal(0.5))
+            self.vuln_score = MonteCarloRange(probable=Decimal(0.1))
+            self.loss_magnitude = MonteCarloSimulation(MonteCarloRange(probable=Decimal(0.001)))
+            self.budget: Decimal=Decimal(1000000),
+            self.currency: str="SEK"
+        else:
+            self.threat_event_frequency = MonteCarloRange.from_dict(values['threat_event_frequency'])
+            self.vuln_score = MonteCarloRange.from_dict(values['vulnerability'])
+            self.loss_magnitude = MonteCarloSimulation(MonteCarloRange.from_dict(values['loss_magnitude']))
+            self.budget = Decimal(values['budget'])
+            self.currency = values['currency']
+            self.loss_event_frequency = MonteCarloSimulation(MonteCarloRange(min=self.threat_event_frequency.min*self.vuln_score.min,
+                                                        max=self.threat_event_frequency.max*self.vuln_score.max,
+                                                        probable=self.threat_event_frequency.probable*self.vuln_score.probable))
+            self.update_ale()
+    
+    def update_ale(self):
+        self.ale = MonteCarloRange(min=self.budget*self.loss_event_frequency.min*self.loss_magnitude.min,
+                                   max=self.budget*self.loss_event_frequency.max*self.loss_magnitude.max,
+                                   probable=self.budget*self.loss_event_frequency.probable*self.loss_magnitude.probable)
+        self.annual_loss_expectancy = MonteCarloSimulation(self.ale)
+
+    def to_dict(self):
+        return {
+            "threat_event_frequency": self.threat_event_frequency.to_dict(),
+            "vulnerability": self.vuln_score.to_dict(),
+            "loss_event_frequency": self.loss_event_frequency.to_dict(),
+            "loss_magnitude": self.loss_magnitude.to_dict(),
+            "annual_loss_expectancy": self.annual_loss_expectancy.to_dict(),
+            "budget": float(self.budget),
+            "currency": self.currency
+        }
+
+    def __repr__(self):
+        return str(self.to_dict())
+    
+    def __str__(self):
+        return str("ALE (p90): " + str(round(self.annual_loss_expectancy.p90, 2)) + f" {self.currency}/år\nALE (p50): " + str(round(self.annual_loss_expectancy.probable, 2)) + f" {self.currency}/år\n")
