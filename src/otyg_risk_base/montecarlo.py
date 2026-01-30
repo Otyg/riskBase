3bbc57a56a2b16688d8bfe42a717cc41be32db16
Martin Vesterlund <Otyg@users.noreply.github.com>
2026-01-30 10:32:37 +0100

    More tests (#5)
    * Tester för samtliga klasser
    
    * Diverse buggfixar

--- src/otyg_risk_base/montecarlo.py
+++ src/otyg_risk_base/montecarlo.py
@@ -30,7 +30,6 @@
 #
 
 import numpy as np
-import statistics
 from decimal import *
 
 
@@ -54,17 +53,34 @@ class MonteCarloRange():
             "max": float(self.max)
         }
     
-    def add(self, other):
+    def add(self, other = None):
         if isinstance(other, MonteCarloRange):
             max = self.max + other.max
             min = self.min + other.min
             probable = self.probable + other.probable
         else:
-            max = self.max + Decimal(other)
-            min = self.min + Decimal(other)
-            probable = self.probable + Decimal(other)
-        return MonteCarloRange(min=Decimal(min), probable=Decimal(probable), max=Decimal(max))
+            max = self.max + other
+            min = self.min + other
+            probable = self.probable + other
+        result = MonteCarloRange(min=Decimal(min), probable=Decimal(probable), max=Decimal(max))
+        return result
 
+    def sub(self, other = None):
+        if isinstance(other, MonteCarloRange):
+            max = self.max - other.max
+            min = self.min - other.min
+            probable = self.probable - other.probable
+        else:
+            max = self.max - other
+            min = self.min - other
+            probable = self.probable - other
+        if max < min:
+            tmp = max
+            max = min
+            min = tmp
+        result = MonteCarloRange(min=Decimal(min), probable=Decimal(probable), max=Decimal(max))
+        return result
+    
     def multiply(self, other = None):
         if isinstance(other, MonteCarloRange):
             max = self.max * other.max
@@ -74,7 +90,8 @@ class MonteCarloRange():
             max = Decimal(self.max) * Decimal(other)
             min = Decimal(self.min) * Decimal(other)
             probable = Decimal(self.probable) * Decimal(other)
-        return MonteCarloRange(min=Decimal(min), probable=Decimal(probable), max=Decimal(max))
+        result = MonteCarloRange(min=Decimal(min), probable=Decimal(probable), max=Decimal(max))
+        return result
 
     @classmethod
     def from_dict(cls, values:dict=None):
@@ -87,17 +104,12 @@ class MonteCarloRange():
         return str(self.to_dict())
     
     def __hash__(self):
-        return hash(("MonteCarloRange", self.max, self.min, self.probable))
+        return hash((self.min, self.max, self.probable))
     
-    def __eq__(self, value):
-        if not isinstance(value, MonteCarloRange) or value == None:
-            return False
-        elif self.__hash__() == value.__hash__():
+    def __eq__(self, other):
+        if isinstance(other, MonteCarloRange) and self.__hash__() == other.__hash__():
             return True
         return False
-    
-    def __ne__(self, value):
-        return not self.__eq__(value)
 
 class PertDistribution():
     def __init__(self,range: MonteCarloRange):
@@ -112,7 +124,9 @@ class PertDistribution():
 
 
 class MonteCarloSimulation():
-    def __init__(self, range: MonteCarloRange):
+    def __init__(self, range: MonteCarloRange=None):
+        if not range:
+            range = MonteCarloRange(probable=Decimal(1))
         if (range.min == range.max == range.probable):
             self.probable = Decimal(range.probable)
             range.max = Decimal(Decimal(range.probable)+Decimal(0.000000000001))
@@ -120,7 +134,7 @@ class MonteCarloSimulation():
         
         pd = PertDistribution(range=range)
         self.__samples = pd.get()
-        self.probable = Decimal(statistics.mode(self.__samples))
+        self.probable = Decimal(np.mean(self.__samples))
         self.p90 = Decimal(np.percentile(self.__samples, 90))
         self.max = Decimal(np.max(self.__samples))
         self.min = Decimal(np.min(self.__samples))
@@ -147,20 +161,49 @@ class MonteCarloSimulation():
         else:
             range = MonteCarloRange.from_dict(dict)
             return MonteCarloSimulation(range=range)
+    
+    def get_montecarlo_range(self):
+        return MonteCarloRange(min=self.min, probable=self.probable, max=self.max)
         
-    def multiply(self, other=None) -> MonteCarloRange:
+    def multiply(self, other):
         if isinstance(other, MonteCarloSimulation):
-            max = self.max * other.max
-            min = self.min * other.min
-            probable = self.probable * other.probable
+            range = self.get_montecarlo_range().multiply(other=other.get_montecarlo_range())
         else:
-            max = Decimal(self.max) * Decimal(other)
-            min = Decimal(self.min) * Decimal(other)
-            probable = Decimal(self.probable) * Decimal(other)
-        return MonteCarloRange(min=min, probable=probable, max=max)
-
+            range = self.get_montecarlo_range().multiply(other=other)
+        return MonteCarloSimulation(range=range)
+    
+    def add(self, other):
+        if isinstance(other, MonteCarloSimulation):
+            range = self.get_montecarlo_range().add(other=other.get_montecarlo_range())
+        else:
+            range = self.get_montecarlo_range().add(other=other)
+        return MonteCarloSimulation(range=range)
+    
+    def delta(self, other):
+        if isinstance(other, MonteCarloSimulation):
+            range = self.get_montecarlo_range().sub(other=other.get_montecarlo_range())
+        else:
+            range = self.get_montecarlo_range().sub(other=other)
+        return MonteCarloSimulation(range=range)
+    
+    def __add__(self, other):
+        return self.add(other)
+    def __radd__(self, other):
+        return self.add(other)
+    def __mul__(self, other):
+        return self.multiply(other)
+    def __rmul__(self, other):
+        return self.multiply(other)
     def __repr__(self):
         return str(self.to_dict())
     
     def __str__(self):
         return f"min: {str(self.min)} mode: {str(self.probable)} p90: {str(self.p90)} max: {str(self.max)}"
+    
+    def __hash__(self):
+        return hash((self.min, self.probable, self.p90, self.max))
+    
+    def __eq__(self, value):
+        if isinstance(value, MonteCarloSimulation) and self.__hash__() == value.__hash__():
+            return True
+        return False
--- test/test_montecarlo.py
+++ test/test_montecarlo.py
@@ -1,9 +1,7 @@
 from decimal import Decimal
 import unittest
 
-from src.otyg_risk_base.montecarlo import MonteCarloRange
-
-
+from src.otyg_risk_base.montecarlo import MonteCarloRange, MonteCarloSimulation
 
 class TestMonteCarloRange(unittest.TestCase):
     def test_no_arg(self):
@@ -44,6 +42,114 @@ class TestMonteCarloRange(unittest.TestCase):
             "max": 10
         }))
         
+class TestMonteCarloSimulation(unittest.TestCase):
+    def test_creation(self):
+        range = MonteCarloRange(min=1, probable=2, max=3)
+        sim = MonteCarloSimulation(range=range)
+        self.assertAlmostEqual(sim.min, 1, delta=0.1)
+        self.assertAlmostEqual(sim.probable, 2, delta=0.1)
+        self.assertAlmostEqual(sim.max, 3, delta=0.1)
+        self.assertAlmostEqual(sim.p90, Decimal(2.5), delta=0.1)
+    
+    def test_equals(self):
+        sim = MonteCarloSimulation(range=MonteCarloRange(min=1, probable=2, max=3))
+        other = MonteCarloSimulation(range=MonteCarloRange(min=1, probable=2, max=3))
+        self.assertTrue(sim == sim)
+        self.assertFalse(sim == other)
+    
+    def test_serialization_deserialization(self):
+        sim = MonteCarloSimulation(range=MonteCarloRange(min=1, probable=2, max=3))
+        other = MonteCarloSimulation().from_dict(sim.to_dict())
+        self.assertTrue(sim == other)
+    
+    def test_multiplication(self):
+        first = MonteCarloSimulation(range=MonteCarloRange(min=1, probable=2, max=3))
+        second = MonteCarloSimulation(range=MonteCarloRange(min=1, probable=2, max=3))
+        
+        mul = first.multiply(second)
+        mul_oper = first * second
+        mul_roper = second * first
+        threshold = 0.3
+        self.assertAlmostEqual(mul.min, 1, delta=threshold)
+        self.assertAlmostEqual(mul.min, mul_oper.min, delta=threshold)
+        self.assertAlmostEqual(mul.min, mul_roper.min, delta=threshold)
+        self.assertAlmostEqual(mul.probable, 4, delta=1)
+        self.assertAlmostEqual(mul.probable, mul_oper.probable, delta=1)
+        self.assertAlmostEqual(mul.probable, mul_roper.probable, delta=1)
+        self.assertAlmostEqual(mul.max, 9, delta=threshold)
+        self.assertAlmostEqual(mul.max, mul_oper.max, delta=threshold)
+        self.assertAlmostEqual(mul.max, mul_roper.max, delta=threshold)
+        self.assertAlmostEqual(mul.p90, Decimal(7), delta=threshold)
+        self.assertAlmostEqual(mul.p90, mul_oper.p90, delta=threshold)
+        self.assertAlmostEqual(mul.p90, mul_roper.p90, delta=threshold)
+        scalar = 2
+        mul_scalar = first.multiply(scalar)
+        mul_scalar_oper = first * scalar
+        mul_scalar_roper = scalar * first
+        self.assertAlmostEqual(mul_scalar.min, 2, delta=threshold)
+        self.assertAlmostEqual(mul_scalar.min, mul_scalar_oper.min, delta=threshold)
+        self.assertAlmostEqual(mul_scalar.min, mul_scalar_roper.min, delta=threshold)
+        self.assertAlmostEqual(mul_scalar.probable, 4, delta=1)
+        self.assertAlmostEqual(mul_scalar.probable, mul_scalar_oper.probable, delta=1)
+        self.assertAlmostEqual(mul_scalar.probable, mul_scalar_roper.probable, delta=1)
+        self.assertAlmostEqual(mul_scalar.max, 6, delta=threshold)
+        self.assertAlmostEqual(mul_scalar.max, mul_scalar_oper.max, delta=threshold)
+        self.assertAlmostEqual(mul_scalar.max, mul_scalar_roper.max, delta=threshold)
+        self.assertAlmostEqual(mul_scalar.p90, Decimal(5), delta=threshold)
+        self.assertAlmostEqual(mul_scalar.p90, mul_scalar_oper.p90, delta=threshold)
+        self.assertAlmostEqual(mul_scalar.p90, mul_scalar_roper.p90, delta=threshold)
+    
+    def test_addition(self):
+        first = MonteCarloSimulation(range=MonteCarloRange(min=1, probable=2, max=3))
+        second = MonteCarloSimulation(range=MonteCarloRange(min=1, probable=2, max=3))
+        
+        add = first.add(second)
+        add_oper = first + second
+        add_roper = second + first
+        threshold = 0.3
+        self.assertAlmostEqual(add.min, 2, delta=threshold)
+        self.assertAlmostEqual(add.min, add_oper.min, delta=threshold)
+        self.assertAlmostEqual(add.min, add_roper.min, delta=threshold)
+        self.assertAlmostEqual(add.probable, 4, delta=1)
+        self.assertAlmostEqual(add.probable, add_oper.probable, delta=1)
+        self.assertAlmostEqual(add.probable, add_roper.probable, delta=1)
+        self.assertAlmostEqual(add.max, 6, delta=threshold)
+        self.assertAlmostEqual(add.max, add_oper.max, delta=threshold)
+        self.assertAlmostEqual(add.max, add_roper.max, delta=threshold)
+        self.assertAlmostEqual(add.p90, Decimal(5), delta=threshold)
+        self.assertAlmostEqual(add.p90, add_oper.p90, delta=threshold)
+        self.assertAlmostEqual(add.p90, add_roper.p90, delta=threshold)
+        scalar = 2
+        add_scalar = first.add(scalar)
+        add_scalar_oper = first + scalar
+        add_scalar_roper = scalar + first
+        self.assertAlmostEqual(add_scalar.min, 3, delta=threshold)
+        self.assertAlmostEqual(add_scalar.min, add_scalar_oper.min, delta=threshold)
+        self.assertAlmostEqual(add_scalar.min, add_scalar_roper.min, delta=threshold)
+        self.assertAlmostEqual(add_scalar.probable, 4, delta=1)
+        self.assertAlmostEqual(add_scalar.probable, add_scalar_oper.probable, delta=1)
+        self.assertAlmostEqual(add_scalar.probable, add_scalar_roper.probable, delta=1)
+        self.assertAlmostEqual(add_scalar.max, 5, delta=threshold)
+        self.assertAlmostEqual(add_scalar.max, add_scalar_oper.max, delta=threshold)
+        self.assertAlmostEqual(add_scalar.max, add_scalar_roper.max, delta=threshold)
+        self.assertAlmostEqual(add_scalar.p90, Decimal(4), delta=0.6)
+        self.assertAlmostEqual(add_scalar.p90, add_scalar_oper.p90, delta=0.6)
+        self.assertAlmostEqual(add_scalar.p90, add_scalar_roper.p90, delta=0.6)
+    
+    def test_delta(self):
+        first = MonteCarloSimulation(range=MonteCarloRange(min=1, probable=2, max=3))
+        second = MonteCarloSimulation(range=MonteCarloRange(min=2, probable=4, max=6))
+        scalar = 2
+        sub = first.delta(second)
+        sub_scalar = first.delta(scalar)
+        # 1-2 = -1, 2-4 = -2, 3-6 = -3
+        threshold = 0.3
+        self.assertAlmostEqual(sub.min, -3, delta=threshold)
+        self.assertAlmostEqual(sub.probable, -2, delta=threshold)
+        self.assertAlmostEqual(sub.max, -1, delta=threshold)
+        self.assertAlmostEqual(sub_scalar.min, -1, delta=threshold)
+        self.assertAlmostEqual(sub_scalar.probable, 0, delta=threshold)
+        self.assertAlmostEqual(sub_scalar.max, 1, delta=threshold)
 
 if __name__ == '__main__':
     unittest.main()
